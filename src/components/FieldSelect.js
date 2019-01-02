e8081bd448e2dd6b5bbf7b5d067821c8e9ae3fce
eddichen <contacteddichen@gmail.com>
2019-01-02 18:21:24 +0000

    styling the select

--- src/components/FieldSelect.js
+++ src/components/FieldSelect.js
@@ -2,18 +2,33 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const SelectWrapper = styled.div`
+  position: relative;
   border: 1px solid #ccc;
-  border-radius: 3px;
+  border-radius: 0px;
   overflow: hidden;
   width: 50%;
   margin: 0 auto;
+  padding 5px 6px;
+
+  &:after {
+    content: '';
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 0;
+    height: 0;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-top: 10px solid #666;
+  }
 `;
 const StyledSelect = styled.select`
-  padding 5px 6px;
   width: 130%;
   border: none;
   background: transparent;
   font-size: 24px;
+
   &:focus {
     outline: none;
   }
